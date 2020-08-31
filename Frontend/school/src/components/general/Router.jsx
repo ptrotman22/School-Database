9abb2c8099bd4e899d31fb940a1164fb0de1a971
Pierce T <ptrotman22@gmail.com>
2020-08-31 08:13:41 -0400

    BootStrap download

--- Frontend/school/src/components/general/Router.jsx
+++ Frontend/school/src/components/general/Router.jsx
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Login from './Login'
-import Registration from '../employee/Registration'
 import WelcomeComponent from './WelcomeComponent'
-import Newflashcard from './NewStudent'
+import NewStudent from './NewStudent'
 import SchoolDatabase from './SchoolDatabase'
-import Updateflashcard from './Updateflashcard'
+
 
 class Router extends Component {
   render() {
@@ -16,8 +15,6 @@ class Router extends Component {
             <Route exact path="/" component={Login}></Route>
             <Route path="/schooldatabase" component={SchoolDatabase}></Route>
             <Route path="/NewStudent" component={NewStudent}></Route>
-            <Route path="/updateflashcard" component={Updateflashcard}></Route>
-            <Route path="/register" component={Registration}></Route>
             <Route path="/logout" component={WelcomeComponent}></Route>
           </Switch>
         </BrowserRouter>
